a7abac6bd21b8d06ed9b47cffe91e50c5032274f
adys <ada.dys@espeo.eu>
2016-04-13 11:01:36 +0200

    inject a service

--- app/todo-input.ts
+++ app/todo-input.ts
@@ -1,15 +1,21 @@
 import {Component} from "angular2/core";
+import {TodoService} from "./todo-service";
 
 @Component({
     selector: 'todo-input',
     directives: [TodoInput],
     template: `<div>
     <input type="text" #myInput>
-    <button (click)="onClick(myInput.value)">Click me</button>
+    <button (mouseover)="onClick(myInput.value)">Click me</button>
     </div>`
 })
 export class TodoInput{
+    //Injecting
+    constructor(public todoService:TodoService){ //'public' to make todoService accesible in onCLick
+    }
+
     onClick(value){
-        console.log(value);
+        this.todoService.todos.push(value);
+        console.log(this.todoService.todos);
     }
 }
