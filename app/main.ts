45d978082e8af61894fcf438af5665797d08d17a
adys <ada.dys@espeo.eu>
2016-04-13 15:49:23 +0200

    build dropdown

--- app/main.ts
+++ app/main.ts
@@ -2,14 +2,16 @@ import {bootstrap}    from 'angular2/platform/browser';
 import {Component} from 'angular2/core';
 import {TodoInput} from './todo-input';
 import {TodoService} from './todo-service';
-import {TodoList} from './todo-list'
+import {TodoList} from './todo-list';
+import {StatusSelector} from './status-selector';
 
 @Component({
     selector: 'my-app',
-    directives: [TodoInput, TodoList],
+    directives: [TodoInput, TodoList, StatusSelector],
     template: `<div>
     <todo-input></todo-input>
-    <todo-list></todo-list>
+    <status-selector (select)="status = $event"></status-selector>
+    <todo-list [status]="status"></todo-list>
     </div>`
 })
 class App { }
